279b72df9f05fec6494d4360bdba690e8cbb3bbc
Darmo117 <darmo117@gmail.com>
2022-11-29 13:44:22 +0100

    delete 'Gadget-Subpages.js' in favor of 'Gadget-SousPages.js'

--- JS-CSS/Mediawiki:/Gadget-SousPages.js
+++ JS-CSS/Mediawiki:/Gadget-SousPages.js
@@ -1,16 +1,12 @@
 /**
- [[Catégorie:JavaScript du Wiktionnaire|SousPages.js]]
- * Sous-pages
- *
- * Place un bouton "afficher les sous-pages" dans la boîte à outils
- *
+ * Place un bouton "afficher les sous-pages" dans la boite à outils.
  * Auteur initial : Delhovlyn
+ * [[Catégorie:JavaScript du Wiktionnaire|SousPages.js]]
+ * <nowiki>
  */
-mw.loader.using('mediawiki.util', function () {
-  var nsWithoutSubpages = [-1, 0, 6, 14];
-  var ns = mw.config.get('wgNamespaceNumber');
-  if ($.inArray(ns, nsWithoutSubpages) == -1) {
-    var lienSouspages = '/wiki/Special:PrefixIndex/' + mw.config.get('wgPageName') + '/';
-    mw.util.addPortletLink('p-tb', lienSouspages, 'Sous-pages', 't-subpages', 'Sous-pages de cette page');
-  }
+mw.loader.using("mediawiki.util", () => {
+  console.log("Chargement de Gadget-SousPages.js…");
+  const lienSouspages = `/wiki/Special:PrefixIndex/${mw.config.get("wgPageName")}/`;
+  mw.util.addPortletLink("p-tb", lienSouspages, "Sous-pages", "t-subpages", "Sous-pages de cette page");
 });
+// </nowiki>

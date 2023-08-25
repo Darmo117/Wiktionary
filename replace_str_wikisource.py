b89604dc785901facd0b82c974341ef6dd699837
Darmo117 <darmo117@gmail.com>
2023-08-25 23:21:09 +0200

    Atipa

--- replace_str_wikisource.py
+++ replace_str_wikisource.py
@@ -2,18 +2,23 @@ import re
 
 import pywikibot as pwb
 
-pages_name = 'Peletier - Dialogue de l\'ortografe e prononciation françoese'
-pattern = re.compile(r'\s?([:;?!])(?!\w)')
-repl = r'&nbsp;\1'
+pages_name = 'Parépou - Atipa, 1885'
+substitions = [
+    (re.compile(' ([:;?!»])'), r' \1'),
+    (re.compile('« '), '« '),
+]
 
 site = pwb.Site(code='fr', fam='wikisource', user='Darmo117')
 
-indices = [*range(2, 177), *range(179, 236)]
+indices = range(1, 239)
 for i, p in enumerate(indices):
     page = pwb.Page(site, title=f'Page:{pages_name}.djvu/{p}')
     if page.exists():
-        print(f'Page {p} ({i}/{len(indices)})…')
+        print(f'Page {p} ({i + 1}/{len(indices)})…')
         text = page.text
-        page.text = re.sub(pattern, repl, page.text)
+        for pattern, repl in substitions:
+            page.text = re.sub(pattern, repl, page.text)
         if text != page.text:
-            page.save(summary=f'(Automatique) Insertion d’une espace insécable devant [:;?!].')
+            page.save(summary='(Automatique) Ajout d’espaces insécables')
+        else:
+            print('Nothing changed, skipped.')

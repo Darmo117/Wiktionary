3bb7733f404501517c93eb83fcd3cbd377b79d03
Darmo117 <darmo117@gmail.com>
2024-01-18 18:41:48 +0100

    màj script replace_str_wikisource.py pour "André - Tout l'Esperanto en 12 dialogues, 1910.pdf"

--- replace_str_wikisource.py
+++ replace_str_wikisource.py
@@ -2,23 +2,27 @@ import re
 
 import pywikibot as pwb
 
-pages_name = 'Parépou - Atipa, 1885'
-substitions = [
-    (re.compile(' ([:;?!»])'), r' \1'),
-    (re.compile('« '), '« '),
+PAGE_NB = 72
+PAGE_BASE_NAME = "André - Tout l'Esperanto en 12 dialogues, 1910.pdf"
+# MESSAGE = 'Ajout d’espaces insécables'
+MESSAGE = 'Remplacement ù en ŭ'
+SUBSTITUTIONS = [
+    # (re.compile('[  ]([:;?!»])'), r'&nbsp;\1'),
+    # (re.compile('«[  ]'), '«&nbsp;'),
+    (re.compile('([^oO])ù'), r'\1ŭ'),
 ]
 
 site = pwb.Site(code='fr', fam='wikisource', user='Darmo117')
 
-indices = range(1, 239)
+indices = range(1, PAGE_NB + 1)
 for i, p in enumerate(indices):
-    page = pwb.Page(site, title=f'Page:{pages_name}.djvu/{p}')
+    page = pwb.Page(site, title=f'Page:{PAGE_BASE_NAME}/{p}')
     if page.exists():
         print(f'Page {p} ({i + 1}/{len(indices)})…')
         text = page.text
-        for pattern, repl in substitions:
+        for pattern, repl in SUBSTITUTIONS:
             page.text = re.sub(pattern, repl, page.text)
         if text != page.text:
-            page.save(summary='(Automatique) Ajout d’espaces insécables')
+            page.save(summary=f'(Automatique) {MESSAGE}')
         else:
             print('Nothing changed, skipped.')

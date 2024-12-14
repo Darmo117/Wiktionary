1764badd32eda057cff7fd33aa24eec94b725bd6
Darmo117 <darmo117@gmail.com>
2024-12-14 12:57:22 +0100

    add Gadget-wikt.dicotheque-link.js

---
+++ JS-CSS/Mediawiki/Gadget-wikt.dicotheque-link.js
@@ -0,0 +1,27 @@
+/**
+ * (fr) Ajoute un lien vers la Dicothèque si la page contient
+ * une section de langue parmi celles supportées par la Dicothèque.
+ * ----------------------------------------------------------------
+ * (en) Adds a link to the Dicothèque if the page contains at least
+ * one language section among those supported by the Dicothèque.
+ */
+$(function () {
+  console.log("Chargement de Gadget-wikt.dicotheque-link.js…");
+
+  const supportedLanguages = ["fr", "frm", "br", "lorrain", "wa"];
+  if (supportedLanguages.every((code) => $(`#${code}`).length === 0)) {
+    console.log("[Gadget-wikt.dicotheque-link.js] Aucune section de langue supportée n’a été trouvée.");
+    return;
+  }
+
+  const entry = mw.config.get("wgTitle");
+  const element = mw.util.addPortletLink(
+      "p-tb",
+      "https://dicotheque.org/search/" + encodeURIComponent(entry),
+      "Dicothèque",
+      "t-dicotheque",
+      "Voir dans la Dicothèque (s’ouvre dans un nouvel onglet)"
+  );
+  if (element)
+    $(element).find("a").attr("target", "_blank");
+});
